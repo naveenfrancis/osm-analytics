a3e3af4bf1bbb8630b27885e5a97805a90fd6e90
erik <erik.escoffier@gmail.com>
2017-06-26 17:31:35 +0200

    added years selectors

--- app/components/Header/embedHeader.js
+++ app/components/Header/embedHeader.js
@@ -7,7 +7,7 @@ import * as MapActions from '../../actions/map'
 import 'react-select/dist/react-select.css'
 import { compareTimes as timeOptions } from '../../settings/options'
 import { filters } from '../../settings/options'
-// import style from './style.css'
+import DropdownButton from '../DropdownButton'
 
 class EmbedHeader extends Component {
   onFeatureTypeClick(selectedFilters) {
@@ -16,17 +16,49 @@ class EmbedHeader extends Component {
     enabledFilters.filter(filter => selectedFilters.indexOf(filter) === -1).map(this.props.actions.disableFilter)
   }
 
+  onYearChangeClick(isEnd, selectedYears) {
+    const newTimes = []
+    newTimes[0] = (isEnd === true) ? this.props.times[0] : selectedYears[0]
+    newTimes[1] = (isEnd === true) ? selectedYears[0] : this.props.times[1]
+    this.props.actions.setTimes(newTimes)
+  }
+
   render() {
-    // const years = timeOptions.map(timeOption => ({
-    //   label: timeOption.id,
-    //   value: timeOption.timestamp
-    // }))
+    const years = timeOptions.map(timeOption => ({
+      id: timeOption.id,
+      description: timeOption.id
+    }))
+    const yearsList = timeOptions.map(timeOption => timeOption.id)
+
+    const yearStart = this.props.times[0]
+    const yearEnd = this.props.times[1]
+
+    // don't allow year start after year end and vice versa
+    const yearsEndIndex = yearsList.indexOf(yearEnd)
+    const yearsStart = years.slice(0, yearsEndIndex)
+    const yearsStartIndex = yearsList.indexOf(yearStart)
+    const yearsEnd = years.slice(yearsStartIndex + 1)
 
     return (
       <div>
         <span>Before and after.</span>
 
-        {/* years selector: use DropdownButton (already in project) */}
+        <DropdownButton
+          style={{marginRight: '150px'}}
+          options={yearsStart}
+          btnElement={<button title='Select time range'>{yearStart}&ensp;▾</button>}
+          multiple={false}
+          selectedKeys={[yearStart]}
+          onSelectionChange={_.partial(this.onYearChangeClick, false).bind(this)}
+        />
+
+        <DropdownButton
+          options={yearsEnd}
+          btnElement={<button title='Select time range'>{yearEnd}&ensp;▾</button>}
+          multiple={false}
+          selectedKeys={[yearEnd]}
+          onSelectionChange={_.partial(this.onYearChangeClick, true).bind(this)}
+        />
 
         {filters.filter(filter => filter.hidden !== true).map(filter => {
           return <button
@@ -45,7 +77,8 @@ class EmbedHeader extends Component {
 
 function mapStateToProps(state) {
   return {
-    enabledFilters: state.map.filters
+    enabledFilters: state.map.filters,
+    times: state.map.times
   }
 }
 
