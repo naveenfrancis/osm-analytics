5ee28a21dc9d755da97775fe8ce676bfc64af68a
erik <erik.escoffier@gmail.com>
2017-06-26 16:44:58 +0200

    fixed runtime error

--- app/libs/leaflet-mapbox-gl.js
+++ app/libs/leaflet-mapbox-gl.js
@@ -185,6 +185,9 @@ L.MapboxGL = L.Layer.extend({
       // update the map on the next available frame to avoid stuttering
       L.Util.requestAnimFrame(function () {
         // reset the scale and offset
+        if (!this._glMap) {
+          return;
+        }
         L.DomUtil.setTransform(this._glMap._canvas.canvas, offset, 1);
 
         // enable panning once the gl map is ready again
