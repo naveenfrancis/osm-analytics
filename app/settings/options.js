1de9882182599be125085eb1a9ab99b9f3442504
erik <erik.escoffier@gmail.com>
2017-09-28 17:16:04 +0200

    exclude waterways from historical tilesets

--- app/settings/options.js
+++ app/settings/options.js
@@ -34,12 +34,12 @@ export const overlays = [
 ]
 
 export const compareTimes = [
-  { id: '2007', timestamp: new Date('2007-01-01'), except: ['pois'] },
-  { id: '2008', timestamp: new Date('2008-01-01'), except: ['pois'] },
-  { id: '2009', timestamp: new Date('2009-01-01'), except: ['pois'] },
-  { id: '2010', timestamp: new Date('2010-01-01'), except: ['pois'] },
-  { id: '2011', timestamp: new Date('2011-01-01'), except: ['pois'] },
-  { id: '2012', timestamp: new Date('2012-01-01'), except: ['pois'] },
+  { id: '2007', timestamp: new Date('2007-01-01'), except: ['waterways', 'pois'] },
+  { id: '2008', timestamp: new Date('2008-01-01'), except: ['waterways', 'pois'] },
+  { id: '2009', timestamp: new Date('2009-01-01'), except: ['waterways', 'pois'] },
+  { id: '2010', timestamp: new Date('2010-01-01'), except: ['waterways', 'pois'] },
+  { id: '2011', timestamp: new Date('2011-01-01'), except: ['waterways', 'pois'] },
+  { id: '2012', timestamp: new Date('2012-01-01'), except: ['waterways', 'pois'] },
   { id: '2013', timestamp: new Date('2013-01-01'), except: ['waterways', 'pois'] },
   { id: '2014', timestamp: new Date('2014-01-01'), except: ['waterways', 'pois'] },
   { id: '2015', timestamp: new Date('2015-01-01'), except: ['highways', 'waterways', 'pois'] },
