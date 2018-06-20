96c3024ca822cbe801a6c2af43eae5b12c3fa9a4
Martin Raifer <martin.raifer@uni-heidelberg.de>
2018-06-20 11:57:42 +0200

    https doesn't work at the moment for osma tiles served from vizzuality

--- app/settings/settings.js
+++ app/settings/settings.js
@@ -1,8 +1,8 @@
 const mapbox_token = 'pk.eyJ1IjoiaG90IiwiYSI6ImNpbmx4bWN6ajAwYTd3OW0ycjh3bTZvc3QifQ.KtikS4sFO95Jm8nyiOR4gQ'
 
 export default {
-  'vt-source': 'https://osm-analytics.vizzuality.com', // source of current vector tiles
-  'vt-hist-source': 'https://osm-analytics.vizzuality.com', // source of historic vector tiles for compare feature
+  'vt-source': 'http://osm-analytics.vizzuality.com', // source of current vector tiles
+  'vt-hist-source': 'http://osm-analytics.vizzuality.com', // source of historic vector tiles for compare feature
   'map-background-tile-layer': 'https://api.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=' + mapbox_token,
   'map-attribution': '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>',
 }
